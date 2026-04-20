d15d059af4082c13b6d96900d5b771937d78fc9c
4zau <4zauf@mail.ru>
2026-04-20 11:37:10 +0700

    done!

--- guess_example.lisp
+++ guess_example.lisp
@@ -1,19 +1,16 @@
 (print "loading random module...")
 (load-plugin "./plugins/random.so")
 
-
 (if (exists goal) (return_nil) (def goal (% (get_random) 100)))
 (print "goal exists")
 
 (print "checking your 'answer'")
 (if (exists answer)
-    (
-        (if (= answer goal) 
-            (print "correct!")
-            (if (> answer goal) 
-                (print "answer too high")
-                (print "answer too low")
-            )
+    (if (= answer goal) 
+        (print "correct!")
+        (if (> answer goal) 
+            (print "answer too high")
+            (print "answer too low")
         )
     )
     (print "please define an 'answer'")
